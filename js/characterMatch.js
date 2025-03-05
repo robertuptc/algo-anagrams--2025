cc35b0446927aa2c413119e78612bad6d94fe0eb
robertuptc <robertuptc@gmail.com>
2025-03-05 18:34:01 -0500

    js problem finished

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,8 +1,31 @@
-exports.isCharacterMatch = function(string1, string2) {
+function isCharacterMatch(string1, string2) {
+    let arrString1 = string1.toLowerCase().split("");
+    let arrString2 = string2.toLowerCase().split("");
+    let comparedWord = []
 
+
+    for (i = 0; i < arrString1.length; i++) {
+        for (j = 0; j < arrString2.length; j++) {
+            if (arrString1[i] == arrString2[j]) {
+                comparedWord.push(arrString2[j])
+            }
+        }
+    }
+    return comparedWord.length == arrString1.length
+}
+
+function anagramsFor(word, listOfWords) {
+    let sortedWord = word.toLowerCase().split("").sort().join('');
+
+    let anagrams = listOfWords.filter((w) => {
+        let sortedW = w.toLowerCase().split("").sort().join('');
+        // console.log(">>>", sortedWord, sortedW)
+        if (sortedW == sortedWord) {
+            return w
+        }
+    })
+    return anagrams
 };
 
-exports.anagramsFor = function(word, listOfWords) {
-
-};
-
+// anagramsFor("saltier", ["cognac", "saltier", "realist", "retails"])
+module.exports = { isCharacterMatch, anagramsFor };
--- js/characterMatchSpec.js
+++ js/characterMatchSpec.js
@@ -1,13 +1,13 @@
-var ana = require("./characterMatch");
+var ana = require("./characterMatch.js");
 
 // Part 1
-console.log(ana.isCharacterMatch('charm', 'march') === true);
-console.log(ana.isCharacterMatch('zach', 'attack') === false);
-console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
-console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
+// console.log(ana.isCharacterMatch('charm', 'march') === true);
+// console.log(ana.isCharacterMatch('zach', 'attack') === false);
+// console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
+// console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
 
-console.log("This test is for the challenge quesiton");
-console.log(ana.isCharacterMatch('Anna Madrigal', 'A man and a girl') === true);
+// console.log("This test is for the challenge quesiton");
+// console.log(ana.isCharacterMatch('Anna Madrigal', 'A man and a girl') === true);
 
 
 // Part 2
@@ -19,4 +19,4 @@ console.log(ana.anagramsFor("threads", listOfWords)[1] == "trashed");
 console.log(ana.anagramsFor("threads", listOfWords)[2] == "hardest");
 console.log(ana.anagramsFor("threads", listOfWords)[3] == "hatreds");
 
-console.log(ana.anagramsFor("apple", listOfWords).length == 0);
+// console.log(ana.anagramsFor("apple", listOfWords).length == 0);
